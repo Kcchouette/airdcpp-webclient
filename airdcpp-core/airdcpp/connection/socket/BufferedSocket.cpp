08b6980b0c6fd8d41f35df81b7062704860ed9a6
Kcchouette <Kcchouette@users.noreply.github.com>
2026-05-24 16:07:09 +0000

    Replace socket write with socket read operation

--- airdcpp-core/airdcpp/connection/socket/BufferedSocket.cpp
+++ airdcpp-core/airdcpp/connection/socket/BufferedSocket.cpp
@@ -376,7 +376,7 @@ void BufferedSocket::threadSendFile(InputStream* file) {
 				writeSize = min(sockSize / 2, writeBufTmp.size() - writePos);
 				written = useLimiter ? 
 					ThrottleManager::getInstance()->write(sock.get(), &writeBufTmp[writePos], writeSize) : 
-					sock->write(&writeBufTmp[writePos], writeSize);
+					sock->read(&inbuf[0], inbuf.size());
 			}
 			
 			if(written > 0) {
